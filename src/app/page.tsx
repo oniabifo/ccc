fb24603bf783ed0d2fe8ffe30ca006981952c871
Howard Irabor <howard@Howards-MacBook-Pro.local>
2024-09-11 16:44:32 -0400

    making changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2247,7 +2247,7 @@ export default function Home() {
                     Is Our Company Certifed ?
                   </h3>
                   <p className="text-base text-body-color dark:text-dark-6">
-                    Lorem ipsum dolor sit amet, contur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Loremolor sit amet, contur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                   </p>
                 </div>
               </div>
